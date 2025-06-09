3d2aaea66b379204d6f766a5711a1a63d0ee6e54
Volodymyr Buhlak <suedanny2@yahoo.com>
2025-06-09 22:13:55 +0300

    init

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,7 @@
+import axios from "axios"
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+
+const API_KEY = "50767734-ec8f632e6fcf50a626b02f32f";
