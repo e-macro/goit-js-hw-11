b1ff66de4db385dc4175c373e37e644e1f951e63
e-macro <suedanny2@yahoo.com>
2025-06-13 15:42:30 +0000

    Deploying to gh-pages from @ e-macro/goit-js-hw-11@22891b4b52b3b1e6f76c5b19be7c9be232785746 🚀

--- index.js
+++ index.js
@@ -1,44 +1,25 @@
-import{a as p,S as u,i as r}from"./assets/vendor-CrlV4O_2.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const a of e)if(a.type==="childList")for(const n of a.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&l(n)}).observe(document,{childList:!0,subtree:!0});function i(e){const a={};return e.integrity&&(a.integrity=e.integrity),e.referrerPolicy&&(a.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?a.credentials="include":e.crossOrigin==="anonymous"?a.credentials="omit":a.credentials="same-origin",a}function l(e){if(e.ep)return;e.ep=!0;const a=i(e);fetch(e.href,a)}})();function m(t){const s="50767734-ec8f632e6fcf50a626b02f32f",i=new URLSearchParams({key:s,q:t,image_type:"photo",orientation:"horizontal",safesearch:!0});return p.get("https://pixabay.com/api",i).then(l=>l.data)}function f(t){return t.map(({webformatURL:s,largeImageURL:i,tags:l,likes:e,views:a,comments:n,downloads:c})=>`
+import{a as p,S as f,i as n}from"./assets/vendor-CrlV4O_2.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&o(i)}).observe(document,{childList:!0,subtree:!0});function a(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=a(e);fetch(e.href,t)}})();function m(r){const s="50767734-ec8f632e6fcf50a626b02f32f",a=new URLSearchParams({key:s,q:r,image_type:"photo",orientation:"horizontal",safesearch:"true"});return p.get("https://pixabay.com/api",{params:a}).then(o=>o.data)}const l=document.querySelector(".loader"),c=document.querySelector(".gallery"),g=new f(".gallery a",{captionDelay:250,captionPosition:"bottom",captionsData:"alt"});function h(r){const s=r.map(({webformatURL:a,largeImageURL:o,tags:e,likes:t,views:i,comments:u,downloads:d})=>`
     <li class="image-box">
-        <img src="${s}" alt="${l}" data-source:"${i}" width="300">
-        <ul class="image-stats">
-            <li class="image-stat">
+        <a href="${o}">
+        <img src="${a}" alt="${e}" width="300">
+        </a>
+        <div class="image-stats">
+            <div class="image-stat">
                 <span class="label">Likes</span>
-                <span class="value">${e}</span>
-            </li>
-            <li class="image-stat">
+                <span class="value">${t}</span>
+            </div>
+            <div class="image-stat">
                 <span class="label">Views</span>
-                <span class="value">${a}</span>
-            </li>
-            <li class="image-stat">
+                <span class="value">${i}</span>
+            </div>
+            <div class="image-stat">
                 <span class="label">Comments</span>
-                <span class="value">${n}</span>
-            </li>
-            <li class="image-stat">
+                <span class="value">${u}</span>
+            </div>
+            <div class="image-stat">
                 <span class="label">Downloads</span>
-                <span class="value">${c}</span>
-            </li>
-        </ul>
-    </li>`).join("")}function d(t){return t.map(()=>`
-    <li class="image-box">
-        <span class="loader"></span>
-        <ul class="image-stats">
-            <li class="image-stat">
-                <span class="label">Likes</span>
-                <span class="value">${likes}</span>
-            </li>
-            <li class="image-stat">
-                <span class="label">Views</span>
-                <span class="value">${views}</span>
-            </li>
-            <li class="image-stat">
-                <span class="label">Comments</span>
-                <span class="value">${comments}</span>
-            </li>
-            <li class="image-stat">
-                <span class="label">Downloads</span>
-                <span class="value">${downloads}</span>
-            </li>
-        </ul>
-    </li>`).join("")}new u(".gallery a",{captionDelay:250,captionPosition:"bottom",captionsData:"alt"});const g=document.querySelector(".form");document.querySelector(".image-box");const o=document.querySelector(".gallery");g.addEventListener("submit",t=>{t.preventDefault(),t.target.elements["search-text"].value,o.innerHTML(d()),m().then(s=>{s.data||r.error({message:"Sorry, there are no images matching your search query. Please try again!",closeOnClick:!0,position:"topRight"}),o.innerHTML(f(s.data))}).catch(s=>r.error({message:`${s}`}))});
+                <span class="value">${d}</span>
+            </div>
+        </div>
+    </li>`).join("");c.innerHTML=s,g.refresh()}function y(){c.innerHTML=""}function v(){l.classList.remove("hidden")}function L(){l.classList.add("hidden")}const b=document.querySelector(".form");document.querySelector(".gallery");b.addEventListener("submit",r=>{r.preventDefault();const s=r.target.elements["search-text"].value;y(),v(),m(s).then(a=>{if(!a.hits.length){n.error({message:"Sorry, there are no images matching your search query. Please try again!",closeOnClick:!0,position:"topRight"});return}h(a.hits)}).catch(a=>n.error({message:`${a}`})).finally(()=>L())});
 //# sourceMappingURL=index.js.map
