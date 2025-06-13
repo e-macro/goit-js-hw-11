5bc6a019e416ddb6bb66bbfb1afbc381d022a10c
Volodymyr Buhlak <suedanny2@yahoo.com>
2025-06-13 18:42:28 +0300

    .

--- src/main.js
+++ src/main.js
@@ -3,13 +3,8 @@ import createGallery, { clearGallery, showLoader, hideLoader } from "./js/render
 import iziToast from "izitoast";
 // Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
-// Описаний у документації
-import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
-import "simplelightbox/dist/simple-lightbox.min.css";
 
 const form = document.querySelector(".form");
-const gallery = document.querySelector(".gallery")
 
 form.addEventListener('submit', (event) => {
     
