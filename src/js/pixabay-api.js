9f7c3ad58735859894beb6ed121050bc5812f9fd
Volodymyr Buhlak <suedanny2@yahoo.com>
2025-06-13 19:46:10 +0300

    .

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,14 +3,21 @@ import axios from "axios";
 export default function getImagesByQuery(query) {
 const API_KEY = "50767734-ec8f632e6fcf50a626b02f32f";
 
-const params = new URLSearchParams({
+
+// const params = new URLSearchParams({
+//     key: API_KEY,
+//     q: query,
+//     image_type: "photo",
+//     orientation: "horizontal",
+//     safesearch: true,
+// });
+
+return axios.get(`https://pixabay.com/api`, {
     key: API_KEY,
     q: query,
     image_type: "photo",
     orientation: "horizontal",
     safesearch: true,
-});
-
-return axios.get(`https://pixabay.com/api?${params}/`)
+})
         .then(response => response.data)
 }
